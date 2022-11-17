0c3c54c8bf461858b529069099107876726ef8a5
MisionTIC <carito2013005@hotmail.com>
2022-11-17 08:41:05 -0500

    ActualizacionMerqueAppMovil2

--- app/src/main/java/com/example/kitkat/MainActivity.java
+++ app/src/main/java/com/example/kitkat/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kitkat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,27 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
     TextInputEditText mTextInputEditTextEmail;
     TextInputEditText mTextInputEditTextPassword;
     Button mButtonLogin;
+    SignInButton mbtngoogle;
+    FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private final int REQUEST_CODE_GOOGLE=1;
+    FirebaseFirestore mFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +52,23 @@ public class MainActivity extends AppCompatActivity {
         mTextInputEditTextEmail=findViewById(R.id.textInputEditTextEmail);
         mTextInputEditTextPassword=findViewById(R.id.textInputEditTextPassword);
         mButtonLogin=findViewById(R.id.btnlogin);
+        mbtngoogle=findViewById(R.id.btnloginSignInGoogle);
+
+        mAuth=FirebaseAuth.getInstance();
+        mFirestore=FirebaseFirestore.getInstance();
+
+        mbtngoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInGoogle();
+            }
+        });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         mButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,10 +88,100 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void signInGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, REQUEST_CODE_GOOGLE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == REQUEST_CODE_GOOGLE) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("ERROR", "Google sign in failed", e);
+            }
+        }
+    }
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            String id = mAuth.getCurrentUser().getUid();
+                            checkUserExist(id);
+                            // inicio de sesion correcto, actualice la interfaz de usuario con la
+                            // informacion del usuario que inicio sesion
+
+                        } else {
+
+                            Log.w("Error", "signInWithCredential:failure", task.getException());
+                        }
+
+                    }
+                });
+    }
+
+    private void checkUserExist(final String id) {
+        mFirestore.collection("Users").document(id).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists()){
+                    Intent intent=new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }else{
+                    String email=mAuth.getCurrentUser().getEmail();
+                    Map<String, Object>map=new HashMap<>();
+                    map.put("email", email);
+                    mFirestore.collection("Users").document(id).set(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                           if(task.isSuccessful()){
+                               Intent intent=new Intent(MainActivity.this,CompleteProfileActivity.class);
+                               startActivity(intent);
+
+                           }else{
+                               Toast.makeText(MainActivity.this, "No se pudo almacenar el usuario", Toast.LENGTH_SHORT).show();
+                           }
+                        }
+                    });
+                }
+
+            }
+        });
+
+    }
+
+
     private void login() {
 
         String email=mTextInputEditTextEmail.getText().toString();
         String password=mTextInputEditTextPassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent =new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, "El email y conraseña no son correctos", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
         Log.d("campo","email"+email);
         Log.d("campo","password"+password);
 
--- app/src/main/java/com/example/kitkat/RegisterActivity.java
+++ app/src/main/java/com/example/kitkat/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.kitkat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,8 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,6 +30,8 @@ public class RegisterActivity extends AppCompatActivity {
     TextInputEditText mTextInputEditTextPasswordR;
     TextInputEditText mTextInputEditTextConfirmPassword;
     Button mButtonRegister;
+    FirebaseAuth mAut;
+    FirebaseFirestore mFirestore;
 
 
     @Override
@@ -36,6 +46,9 @@ public class RegisterActivity extends AppCompatActivity {
         mTextInputEditTextPasswordR=findViewById(R.id.textInputEditTextPasswordR);
         mTextInputEditTextConfirmPassword=findViewById(R.id.textInputEditTextConfirmPassword);
         mButtonRegister=findViewById(R.id.btnregister);
+
+        mAut=FirebaseAuth.getInstance();
+        mFirestore=FirebaseFirestore.getInstance();
 
         mButtonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,20 +72,65 @@ public class RegisterActivity extends AppCompatActivity {
     private void register() {
         String username=mTextInputEditTextUsername.getText().toString();
         String email=mTextInputEditTextEmailR.getText().toString();
-        String password=mTextInputEditTextPassworR.getText().toString();
-        String confirpassword=mTextInputEditTextConfirmPassword.getText().toString();
+        String password=mTextInputEditTextPasswordR.getText().toString();
+        String confirmpassword=mTextInputEditTextConfirmPassword.getText().toString();
 
-        if(!username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !confirpassword.isEmpty()){
+        if (!username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !confirmpassword.isEmpty()){
             if (isEmailValid(email)){
-                Toast.makeText(this, "insertaste todos los campos y el correo es valido", Toast.LENGTH_SHORT).show();
-            }else{
-                Toast.makeText(this, "Has insertado todos los cambios pero el correo no es valido", Toast.LENGTH_SHORT).show();
-                
+                if(password.equals(confirmpassword)){
+                    if(password.length() >=6){
+                        createUser(username,email,password);
+                    }else {
+                        Toast.makeText(this, "Las contraseñas debe tener 6 caracteres", Toast.LENGTH_SHORT).show();
+                    }
+
+                }else{
+                    Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+                }
+
+            }else {
+                Toast.makeText(this, "Has insertado todos los campos y el correo no es valido", Toast.LENGTH_SHORT).show();
             }
-           
-        }else{
+
+
+        }else {
             Toast.makeText(this, "para continuar inserta todos los campos", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void createUser(final String username, final String email, String password) {
+        mAut.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    String id=mAut.getCurrentUser().getUid();
+                    Map<String,Object> map=new HashMap<>();
+                    map.put("email",email);
+                    map.put("username",username);
+                    map.put("password",password);
+                    mFirestore.collection("Users").document(id).set(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this, "El usuario se almaceno correctamente", Toast.LENGTH_SHORT).show();
+
+
+                            }else{
+                                Toast.makeText(RegisterActivity.this, "No se pudo almacenar en la base de datos", Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        }
+                    });
+
+                    Toast.makeText(RegisterActivity.this, "El usuario se registro correctamente", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(RegisterActivity.this, "No se pudo regisrar el usuario ", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 
     public boolean isEmailValid(String email) {
