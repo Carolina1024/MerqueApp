fe0c3edeabec301cbf8175e6de61fd9b775a9f9f
MisionTIC <carito2013005@hotmail.com>
2022-11-28 18:31:09 -0500

    Semana5sesion2ActivityPostCompleto

--- app/src/main/java/com/example/kitkat/activities/HomeActivity.java
+++ app/src/main/java/com/example/kitkat/activities/HomeActivity.java
@@ -1,16 +1,56 @@
 package com.example.kitkat.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.kitkat.R;
+import com.example.kitkat.fragment.ChatsFragment;
+import com.example.kitkat.fragment.FiltersFragment;
+import com.example.kitkat.fragment.HomeFragment;
+import com.example.kitkat.fragment.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigation=findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(new HomeFragment());
     }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    if(item.getItemId()==R.id.itemHome){      // aqui se abre un fragmente home
+                        openFragment(new HomeFragment());
+                    }else if(item.getItemId()==R.id.itemFilter){   // aqui se abre un fragmente filtros
+                        openFragment(new FiltersFragment());
+                    }else if (item.getItemId()==R.id.itemChats){      // aqui se abre un fragmente chats
+                        openFragment(new ChatsFragment());
+                    }else if (item.getItemId()==R.id.itemPerfil){      // aqui se abre un fragmente perfil
+                        openFragment(new ProfileFragment());
+                    }
+                    return true;
+                }
+            };
+
 }
---
+++ app/src/main/java/com/example/kitkat/activities/PostActivity.java
@@ -0,0 +1,16 @@
+package com.example.kitkat.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kitkat.R;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
